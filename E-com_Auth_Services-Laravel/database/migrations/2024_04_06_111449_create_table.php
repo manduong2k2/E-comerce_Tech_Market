3b15d0631b45b4056f5918ece22574260f6437a8
manduong2k2 <manduong2k2@gmail.com>
2024-10-21 15:17:15 +0700

    Update

--- E-com_Auth_Services-Laravel/database/migrations/2024_04_06_111449_create_table.php
+++ E-com_Auth_Services-Laravel/database/migrations/2024_04_06_111449_create_table.php
@@ -84,62 +84,6 @@ return new class extends Migration
             $table->timestamp('expires_at')->nullable();
             $table->timestamps();
         });
-        Schema::create('products', function (Blueprint $table) {
-            $table->bigIncrements('id');
-            $table->string('name');
-            $table->string('image');
-            $table->integer('stock')->unsigned();
-            $table->integer('price')->unsigned();
-            $table->text('description');
-            $table->timestamps();
-
-            $table->unsignedBigInteger('brand_id');
-            $table->foreign('brand_id')->references('id')->on('brands')->onDelete('cascade');
-
-            $table->unsignedBigInteger('category_id');
-            $table->foreign('category_id')->references('id')->on('categories')->onDelete('cascade');
-
-            $table->foreignId('user_id')->nullable()->index()->constrained()->onDelete('cascade');
-        });
-        Schema::create('carts', function (Blueprint $table) {
-            $table->unsignedBigInteger('user_id');
-            $table->unsignedBigInteger('product_id');
-            $table->unsignedBigInteger('quantity');
-
-
-            $table->primary(['user_id', 'product_id']);
-
-            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
-            $table->foreign('product_id')->references('id')->on('products')->onDelete('cascade');
-
-            $table->timestamps();
-        });
-        Schema::create('orders', function (Blueprint $table) {
-            $table->bigIncrements('id');
-            $table->date('date');
-            $table->unsignedBigInteger('user_id');
-            $table->unsignedBigInteger('total');
-
-            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
-
-            $table->timestamps();
-        });
-
-        Schema::create('items', function (Blueprint $table) {
-            $table->unsignedBigInteger('order_id');
-            $table->unsignedBigInteger('product_id');
-
-            $table->unsignedBigInteger('cost');
-            $table->unsignedBigInteger('rating');
-            $table->unsignedBigInteger('quantity');
-
-            $table->primary(['order_id', 'product_id']);
-
-            $table->foreign('order_id')->references('id')->on('orders')->onDelete('cascade');
-            $table->foreign('product_id')->references('id')->on('products')->onDelete('cascade');
-
-            $table->timestamps();
-        });
     }
 
     /**
@@ -147,14 +91,7 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('items');
-        Schema::dropIfExists('orders');
-        Schema::dropIfExists('carts');
         Schema::dropIfExists('personal_access_tokens');
-        Schema::dropIfExists('product_category');
-        Schema::dropIfExists('products');
-        Schema::dropIfExists('categories');
-        Schema::dropIfExists('brands');
         Schema::dropIfExists('password_reset_tokens');
         Schema::dropIfExists('sessions');
         Schema::dropIfExists('role_user');
